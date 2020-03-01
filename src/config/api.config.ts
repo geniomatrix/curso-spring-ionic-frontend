16d2a2aeeb24fc83ddd558cb4c5d31aab3c644c5
ArcanjoWeb <geniomatrix@hotmail>
2020-03-01 12:05:53 -0300

    Instalação dos requisitos para build Android

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "https://app-aw-ionic.herokuapp.com",
+    baseUrl: "https://curso-ionic-app.herokuapp.com",
     bucketBaseUrl: "https://arcanjoweb.s3-sa-east-1.amazonaws.com"
     
 }
